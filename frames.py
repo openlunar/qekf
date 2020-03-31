Fixed planet direction of rotation

frames.py
@@ -90,13 +90,16 @@ def compute_T_inrtl_to_pcpf(dt,
     Returns:
         A 3x3 transformation matrix.
     """
+
+    # The negative sign inside rotate_z controls the direction of
+    # rotation.
     if T_inrtl_to_pcpf0 is None:
         if dt == 0:
             return np.identity(3)
-        return rotate_z(w_pcpf * dt)
+        return rotate_z(-w_pcpf * dt)
     else:
         if dt == 0:
             return T_inrtl_to_pcpf0
-        return rotate_z(w_pcpf * dt).dot(T_inrtl_to_pcpf0)
+        return rotate_z(-w_pcpf * dt).dot(T_inrtl_to_pcpf0)
     
     

sim_flatsat.py
@@ -16,8 +16,9 @@ class SimFlatsat(object):
                  T_enu_to_body = np.identity(3)):
 
         self.t                = 0.0
-        self.w_pcpf_wrt_inrtl = np.array([0.0, 0.0,
-                                          2.0 * np.pi / (23 * 3600.0 + 56 * 60.0 + 4.091)])
+        self.w_pcpf_wrt_inrtl = np.array([ 0.0,
+                                           0.0,
+                                           2.0 * np.pi / (23 * 3600.0 + 56 * 60.0 + 4.091)])
         self.w_body_wrt_pcpf  = np.zeros(3)
         self.r_pcpf           = planetodetic_to_pcpf(lon, lat)
         self.T_inrtl_to_pcpf  = np.identity(3)